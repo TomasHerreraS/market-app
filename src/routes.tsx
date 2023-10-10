dccc4dd64d181ac5f7714700bd8eb6c11ad50644
Tomás <tomas.edhs@gmail.com>
2023-10-10 10:40:19 -0300

    First push market app

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+// import Mainpage from 'pages/...'
+
+const RouteByComponent = () => {
+//   TODO: INSERT BELOW ROUTE BY CALLING COMPONENT FROM PAGE
+//   Example:
+//   return (
+//     <Routes>
+//       <Route path='/' element={<MainPage/>} />
+//     </Routes>
+//   )
+}
+
+export default RouteByComponent;
