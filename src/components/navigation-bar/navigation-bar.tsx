2a63f52bcd54d76b86e7143b81ddb723121a2ee3
Tomás <tomas.edhs@gmail.com>
2023-11-13 08:28:33 -0300

    navbar update

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { Row, Col } from "react-bootstrap";
+import {useState} from 'react'
+import { Row, Col, Stack } from "react-bootstrap";
 import SignIn from '../sign-in/sign-in';
 import SignUp from '../sign-up/sign-up';
 import '../../styles/navigation-bar.css';
@@ -12,14 +12,14 @@ const NavigationBar = () => {
   const [showSignUp, setShowSignUp] = useState<boolean>(false)
 
   return (
-    <Row className="background-color text-color padding">
+    <Row className="background-color text-color padding g-0 align-items-center">
       <Col md={1}>
         <Peripherals/>
       </Col>
       <Col md={1}>
         <Component/>
       </Col>
-      <Col className='col-input' md={{ span: 3, offset: 1}}>
+      <Col className='col-input' md={{ span: 2, offset: 1}}>
         <div style={{ position: 'relative' }}>
           <FaSearch className='search-icon'/>
           <input
@@ -29,25 +29,25 @@ const NavigationBar = () => {
           />
         </div>
       </Col>
-      <Col md={{ span: 1, offset: 4}}>
-        <h6
-          className='cursor-pointer h6-margin'
-          onClick={()=>{
-            setShowSignUp(true);
-          }}>
-          Sign up
-        </h6>
-        <SignUp show={showSignUp} setShow={setShowSignUp} />
-      </Col>
-      <Col md={1}>
-        <h6
-          className='cursor-pointer h6-margin'
-          onClick={()=>{
-            setShowSignIn(true);
-          }}>
-          Sign in
-        </h6>
-        <SignIn show={showSignIn} setShow={setShowSignIn}/>
+      <Col md={{span: 2, offset: 5}}>
+        <Stack direction='horizontal' gap={4}>
+          <h6
+            className='cursor-pointer h6-margin'
+            onClick={()=>{
+              setShowSignUp(true);
+            }}>
+            Sign up
+          </h6>
+          <SignUp show={showSignUp} setShow={setShowSignUp} />
+          <h6
+            className='cursor-pointer h6-margin'
+            onClick={()=>{
+              setShowSignIn(true);
+            }}>
+            Sign in
+          </h6>
+          <SignIn show={showSignIn} setShow={setShowSignIn}/>
+        </Stack>
       </Col>
     </Row>
   )
--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -3,11 +3,12 @@ import ProductCarousel from "../components/carousel/explore-product-carousel";
 import ProductBoxRow from "../components/product-box/product-box-row";
 import Footer from "../components/footer/footer";
 import "../styles/main-page.css";
+import NavigationBar from "../components/navigation-bar/navigation-bar";
 
 const MainPage = () => {
   return (
     <>
-      {/* <NavigationBar /> */}
+      <NavigationBar />
       <Slideshow />
       <ProductCarousel />
       <ProductBoxRow />
